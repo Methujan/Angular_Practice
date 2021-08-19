be1e57497139c41731f364c1eb8ee7a5d5fdf679
Methujan <methujan_02@hotmail.com>
2021-08-18 21:56:53 -0400

    add last name

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Methujan';
+  first_name = 'Methujan';
+  last_name = 'Jeyapalan';
+  
 }
