edb177dd39a7b05631126c75d997eef6fb6732f0
Methujan <methujan_02@hotmail.com>
2021-08-24 15:51:32 -0400

    change back to single name

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  first_name = 'Methujan'; //first name
-  last_name = 'Jeyapalan'; //last name
-  
+  name = 'Methujan'; //first name
+
+
 }
