001a5cde4ba20be4e0e2c0de842a0e8cc910f846
Methujan <methujan_02@hotmail.com>
2021-08-31 20:00:22 -0400

    save inputted server name as serverName

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName='';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,7 +24,8 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
   }
-  onUpdateServerName(){
-    
+  onUpdateServerName(event: any){
+    console.log(event)
+    this.serverName= (<HTMLInputElement>event.target).value;
   }
 }
