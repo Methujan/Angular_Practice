d0afc1cb4965e31a7d01482af470ad981ba181e5
Methujan <methujan_02@hotmail.com>
2021-09-07 22:18:09 -0400

    add to server list when button is pressed

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -25,6 +25,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: any){
